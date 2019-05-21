3c78484977092fe6dbae9345bee775e68b3ead70
julianportelli <portelli.jul@gmail.com>
2019-05-21 11:35:14 +0200

    app now supports deletion of posts

--- app/src/main/java/com/example/cis2208/PostsActivity.java
+++ app/src/main/java/com/example/cis2208/PostsActivity.java
@@ -9,11 +9,14 @@ import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,13 +28,17 @@ public class PostsActivity extends AppCompatActivity implements PostAdapter.OnIt
 
     private ProgressBar mProgressCircle;
 
+    private FirebaseStorage mStorage;
     private DatabaseReference mDatabaseRef;
+    private ValueEventListener mDBListener;
     private List<Post> mPosts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_posts);
+
+        PostsActivity.this.setTitle("All Posts");
 
         mRecyclerview = findViewById(R.id.recycler_viewX);
         mRecyclerview.setHasFixedSize(true);
@@ -40,21 +47,24 @@ public class PostsActivity extends AppCompatActivity implements PostAdapter.OnIt
         mProgressCircle = findViewById(R.id.progress_circle);
 
         mPosts = new ArrayList<>();
+        mAdapter = new PostAdapter(PostsActivity.this, mPosts);
+        mRecyclerview.setAdapter(mAdapter);
+        mAdapter.setOnItemClickListener(PostsActivity.this);
 
+        mStorage = FirebaseStorage.getInstance();
         mDatabaseRef = FirebaseDatabase.getInstance().getReference("posts");
-        mDatabaseRef.addValueEventListener(new ValueEventListener() {
+
+        mDBListener = mDatabaseRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mPosts.clear();
                 for(DataSnapshot ds : dataSnapshot.getChildren()){
                     Post post = ds.getValue(Post.class);
+                    post.setmKey(ds.getKey());
                     mPosts.add(post);
                 }
 
-                mAdapter = new PostAdapter(PostsActivity.this, mPosts);
-                mRecyclerview.setAdapter(mAdapter);
-
-                mAdapter.setOnItemClickListener(PostsActivity.this);
-
+                mAdapter.notifyDataSetChanged();
                 mProgressCircle.setVisibility(View.INVISIBLE);
             }
 
@@ -78,6 +88,21 @@ public class PostsActivity extends AppCompatActivity implements PostAdapter.OnIt
 
     @Override
     public void onDeleteClick(int position) {
-        Toast.makeText(this, "Delete click at position: " + position, Toast.LENGTH_SHORT).show();
+        Post selectedPost = mPosts.get(position); //get post at clicked position
+        final String selectedKey = selectedPost.getmKey();
+        StorageReference imageRef = mStorage.getReferenceFromUrl(selectedPost.getmImageUrl());
+        imageRef.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                mDatabaseRef.child(selectedKey).removeValue();
+                Toast.makeText(PostsActivity.this, "Post deleted", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mDatabaseRef.removeEventListener(mDBListener);
     }
 }
